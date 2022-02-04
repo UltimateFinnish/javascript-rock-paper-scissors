57fe69d16997be1cbed2e8d1a6870913edc13ce3
Ultimate Finnish <ultimate.finnish@gmail.com>
2022-02-04 21:13:23 +0200

    Added comments.

--- javascript.js
+++ javascript.js
@@ -1,15 +1,14 @@
 const playerSelection = prompt("Rock, paper or scissors.");
 
 function computerPlay(){
-    let RPS = ['Rock', 'Paper', 'Scissors'];
-    computerSelection = RPS[Math.floor(Math.random() * RPS.length)];
-    console.log(computerSelection);
+    let RPS = ['Rock', 'Paper', 'Scissors']; // Puts Rock, Paper and scissors into an array.
+    computerSelection = RPS[Math.floor(Math.random() * RPS.length)];    //Adds a random string from RPS-array to computerSelection
     return computerSelection;
 }
 
 playRound();
 
 function playRound(playerSelection, computerSelection) {
-computerPlay();
+computerSelection = computerPlay(); // Grabs a value from function computerPlay and adds it into variable computerSelection
 console.log(computerSelection);
 }
